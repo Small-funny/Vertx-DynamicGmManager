43d71fefb66df819f1f0af07e520923324d0518a
jiahanyu <jiahanyu@fotoable>
2020-08-17 18:10:35 +0800

    pref: Speed up database operations

--- src/main/java/Server/DatabaseHelper/JdbcMysqlHelper.java
+++ src/main/java/Server/DatabaseHelper/JdbcMysqlHelper.java
@@ -10,24 +10,29 @@ import java.util.List;
 public class JdbcMysqlHelper{
 
     //数据库连接初始化方法
-    private static Connection initJdbc() {
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("连接数据库...");
-            conn = DriverManager.getConnection("", "", "");
-            return conn;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    private static Connection initJdbc() {
+//        Connection conn = null;
+//        try {
+//            long starttime = System.currentTimeMillis();
+//            System.out.println("数据库连接计时开始");
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            System.out.println("连接数据库...");
+//            conn = DriverManager.getConnection("jdbc:mysql://cdb-jpp9dqkf.usw.cdb.myqcloud.com:23055/game_manager_test?useSSL=false", "game", "Pxh130529disc-123");
+//            long endtime = System.currentTimeMillis();
+//            System.out.println("数据库连接运行时间：" + (endtime - starttime) + "ms");
+//            return conn;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
     //登录查询，是否允许用户登录
     public static boolean isExisted(String username, String password) {
         boolean result = false;
-        Connection conn = initJdbc();
+        Connection conn = DBPool.getConnection();
         try {
+
             System.out.println("实例化PreparedStatement对象...");
             PreparedStatement stmt = conn.prepareStatement("select username from user where password = '" + password
                     + "' and username = '" + username +"'");
@@ -35,6 +40,7 @@ public class JdbcMysqlHelper{
             if (rs.next()) {
                 result = true;
             }
+            rs.close();
             stmt.close();
             conn.close();
         } catch (SQLException e) {
@@ -46,17 +52,21 @@ public class JdbcMysqlHelper{
     //查询用户对应的权限
     public static List<String> selectAuthority(String token) {
         List<String> result = new ArrayList<>();
-        Connection conn = initJdbc();
+        Connection conn = DBPool.getConnection();
         try {
             System.out.println("实例化PreparedStatement对象...");
+
+
             PreparedStatement stmt = conn.prepareStatement("select auth.name from auth, role_auth, user_role, user where" +
                     " auth.id = role_auth.auth_id and user_role.role_id = role_auth.role_id" +
                     " and user_role.user_id = user.id and user.token = '" + token + "'");
 
             ResultSet rs = stmt.executeQuery();
+
             while (rs.next()) {
                 result.add(rs.getString("auth.name"));
             }
+            rs.close();
             stmt.close();
             conn.close();
         } catch (SQLException e) {
@@ -67,7 +77,7 @@ public class JdbcMysqlHelper{
 
     public static boolean tokenIsExisted(String token) {
         boolean result = false;
-        Connection conn = initJdbc();
+        Connection conn = DBPool.getConnection();
         try {
             PreparedStatement stmt = conn.prepareStatement("select token from user where token = '" + token
                     + "'");
@@ -75,6 +85,7 @@ public class JdbcMysqlHelper{
             if (rs.next()) {
                 result = true;
             }
+            rs.close();
             stmt.close();
             conn.close();
         } catch (SQLException e) {
@@ -85,7 +96,7 @@ public class JdbcMysqlHelper{
 
     // 适合增删改三种操作
     public static void execute(String sql) {
-        Connection conn = initJdbc();
+        Connection conn = DBPool.getConnection();
         try {
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.execute();
