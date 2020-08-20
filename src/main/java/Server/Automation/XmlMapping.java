f070217266ed6f1ba8f8d9e979779b94ffaeb8a1
wendaohan <wendaohan@fotoable.com>
2020-08-20 16:11:50 +0800

    fix router add server select and auth

--- src/main/java/Server/Automation/XmlMapping.java
+++ src/main/java/Server/Automation/XmlMapping.java
@@ -1,11 +1,11 @@
 package Server.Automation;
 
+import Server.DatabaseHelper.DatabaseHelper;
 import Server.DatabaseHelper.JdbcMysqlHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.jdom2.*;
 import org.jdom2.input.SAXBuilder;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.util.*;
 
@@ -16,15 +16,13 @@ public class XmlMapping {
     private static final HashMap<String, Element> pageElement = new HashMap<>();
     //页面类型是键 对应的类型element是值
     private static final HashMap<String, Element> typeElement = new HashMap<>();
-    private static final Queue<String> iconTiList = new LinkedList<>();
-    private static final Queue<String> iconFaList = new LinkedList<>();
-
+    private static final HashMap<String, Element> serverElement = new HashMap<>();
 
     public XmlMapping() throws JDOMException, IOException {
         SAXBuilder saxBuilder = new SAXBuilder();
         //Element root = saxBuilder.build("src/main/java/resources/properties.xml").getDocument().getRootElement();
         Element root = saxBuilder.build("properties.xml").getDocument().getRootElement();
-        List<Element> typeList = root.getChildren();
+        List<Element> typeList = root.getChild("pages").getChildren();
         for (Element typeE : typeList) {
             typeElement.put(typeE.getAttribute("name").getValue(), typeE);
             List<Element> pageList = typeE.getChildren();
@@ -32,43 +30,13 @@ public class XmlMapping {
                 pageElement.put(pageE.getAttribute("url").getValue(), pageE);
             }
         }
-        iconTiList.add("ti-heart");
-        iconTiList.add("ti-folder");
-        iconTiList.add("ti-export");
-        iconTiList.add("ti-eye");
-        iconTiList.add("ti-crown");
-        iconTiList.add("ti-comment");
-        iconTiList.add("ti-camera");
-        iconTiList.add("ti-zip");
-        iconTiList.add("ti-truck");
-        iconTiList.add("ti-printer");
-        iconFaList.add("fa-safari");
-        iconFaList.add("fa-chrome");
-        iconFaList.add("fa-firefox");
-        iconFaList.add("fa-opera");
-        iconFaList.add("fa-contao");
-        iconFaList.add("fa-industry");
-        iconFaList.add("fa-map");
-        iconFaList.add("fa-vimeo");
-        iconFaList.add("fa-edge");
-        iconFaList.add("fa-modx");
-    }
-
-    private String createIconString(String str) {
-        String iconString;
-        Random random = new Random();
-        if (str.equals("ti")) {
-
-            iconString = iconTiList.poll();
-            //iconTiList.remove(order);
-        } else {
-            int order = random.nextInt(iconFaList.size());
-            iconString = iconFaList.poll();
-            //iconFaList.remove(order);
+        List<Element> serverlist = root.getChild("servers").getChildren();
+        for (Element element : serverlist) {
+            serverElement.put(element.getAttribute("value").getValue(), element);
         }
 
-        return iconString;
     }
+
 
     //创建页面的字符串
     public String createElementString(Element element) {
@@ -87,10 +55,9 @@ public class XmlMapping {
                 //当前标签外层所需要的标签和类
                 if (((Element) child).getName().equals("form")) {
                     stringBuilder.append("<div class=\"card\">");
-                    stringBuilder.append("<div class=\"card-header\"><strong>"+((Element) child).getAttribute("name").getValue()+"</strong></div>");
+                    stringBuilder.append("<div class=\"card-header\"><strong>" + ((Element) child).getAttribute("name").getValue() + "</strong></div>");
                     stringBuilder.append("<div class=\"card-body card-block\">");
-                }
-                else if (((Element) child).getName().equals("formcheck")) {
+                } else if (((Element) child).getName().equals("formcheck")) {
                     stringBuilder.append("<div class=\"row form-group\"><div class=\"col col-md-3\"><label class=\" form-control-label\">").append(((Element) child).getAttribute("name").getValue()).append("</label></div><div class=\"col-12 col-md-9\"><div class=\"form-check\">");
                     //不是option这种小标签的通用类 input select会用
                 } else if (!(((Element) child).getName().equals("option") || ((Element) child).getName().equals("checkbox") || ((Element) child).getName().equals("radio"))) {
@@ -98,9 +65,8 @@ public class XmlMapping {
                 }
 
 
-
                 //添加标签头 checkbox 和radio 不用写属性 有特殊写法 clock也不写 date 也不写
-                if (!(((Element) child).getName().equals("checkbox") || ((Element) child).getName().equals("radio")||((Element) child).getName().equals("clock")||((Element) child).getName().equals("date"))) {
+                if (!(((Element) child).getName().equals("checkbox") || ((Element) child).getName().equals("radio") || ((Element) child).getName().equals("time"))) {
                     stringBuilder.append("<").append(((Element) child).getName());
                     //添加属性
                     for (Attribute attribute : ((Element) child).getAttributes()) {
@@ -112,9 +78,9 @@ public class XmlMapping {
                 ///////根据不同的元素添加特别的属性
 
 
-                //表单特殊属性
+                //表单特殊属性 method="post"
                 if (((Element) child).getName().equals("form")) {
-                    stringBuilder.append(" method=\"post\" class=\"form-horizontal\">");
+                    stringBuilder.append("  class=\"form-horizontal\">");
                     //输入框的特殊属性
                 } else if (((Element) child).getName().equals("input") || ((Element) child).getName().equals("select")) {
                     stringBuilder.append("class=\"form-control\">");
@@ -123,7 +89,7 @@ public class XmlMapping {
                     stringBuilder.append(child.getValue());
                 } else if (((Element) child).getName().equals("formcheck")) {
                     stringBuilder.append("class=\"form-check\">");
-                    for(Element childElement :((Element) child).getChildren()){
+                    for (Element childElement : ((Element) child).getChildren()) {
                         stringBuilder.append("<div class=\"")
                                 .append(childElement.getName())
                                 .append("\"><label  class=\"form-check-label \"><input type=\"radio\" value=\"")
@@ -133,14 +99,10 @@ public class XmlMapping {
                                 .append(childElement.getValue()).append("</label></div>");
 
                     }
-                }else if(((Element) child).getName().equals("clock")){
-                    stringBuilder.append("<div class=\"input-group clockpicker\" >")
-                            .append("<input type=\"text\" id=\"Clock\"  autocomplete=\"off\" data-autoclose=\"true\" class=\"form-control\" name=\"")
-                            .append(((Element) child).getAttribute("name").getValue()).append("\">")
-                            .append("</div>");
-                }
-                else if (((Element) child).getName().equals("date")){
-                   stringBuilder.append("<input autocomplete=\"off\" type=\"text\"").append(((Element) child).getAttribute("name").getValue()).append(" id=\"Date\" class=\"form-control\">");
+                } else if (((Element) child).getName().equals("time")) {
+                    stringBuilder.append("<input type=\"text\" name=\"")
+                            .append(((Element) child).getName())
+                            .append("\" id=\"TIMESTAMP\" class=\"form-control\" autocomplete=\"off\">");
                 }
 
 
@@ -170,11 +132,6 @@ public class XmlMapping {
 
     }
 
-    //生成text类型对象的字符串
-    public String createTextString(Text text) {
-        return text.getValue();
-    }
-
     //获取页面的element
     public Element getElement(String str) {
         return pageElement.get(str);
@@ -185,43 +142,43 @@ public class XmlMapping {
     }
 
     //生成侧边栏的字符串
-    public String createAsideString() {
-        System.out.println("///////////////////////////////////////////////////////");
-        StringBuilder stringBuilder = new StringBuilder();
-        //这里的外层是一个div，从这里是ul开始
-        stringBuilder.append("<ul class=\"nav navbar-nav\" style=\"width: 280px\" > ");
-        //类别使用name作为序号
-        int name = 1;
-        for (Map.Entry<String, Element> entry : typeElement.entrySet()) {
-            //添加种类名称 每一个种类是li
-            stringBuilder.append("<li class=\"menu-item-has-children dropdown  name= \"" + name + "\" \" >");
-            name++;
-            stringBuilder.append("<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\"\n" +
-                    "aria-expanded=\"false\" ><i class=\"menu-icon fa " + createIconString("fa") + "\"></i>" + entry.getKey() + "</a>");
-            //每一个种类还是一个ul 里面的每个页面是一个li
-            stringBuilder.append(" <ul class=\"sub-menu children dropdown-menu \" >");
-            //System.out.println(entry.getKey());style="padding-left: 0px"
-            int ID = 1;
-            for (Element element : entry.getValue().getChildren()) {
-                if (element.getAttribute("url") != null) {
-                    stringBuilder.append("<li id><i class=\"menu-icon " + createIconString("ti") + "\"></i><a href=\"" + element.getAttribute("url").getValue() + "\">" + element.getAttribute("name").getValue() + "</a></li>");
-                    //System.out.println(element.getAttribute("name").getValue());
-                } else {
-                    stringBuilder.append("<li id><i class=\"menu-icon " + createIconString("ti") + "\"></i><a href=\"#\">" + element.getAttribute("name").getValue() + "</a></li>");
+//    public String createAsideString() {
+//        System.out.println("///////////////////////////////////////////////////////");
+//        StringBuilder stringBuilder = new StringBuilder();
+//        //这里的外层是一个div，从这里是ul开始
+//        stringBuilder.append("<ul class=\"nav navbar-nav\" style=\"width: 280px\" > ");
+//        //类别使用name作为序号
+//        int name = 1;
+//        for (Map.Entry<String, Element> entry : typeElement.entrySet()) {
+//            //添加种类名称 每一个种类是li
+//            stringBuilder.append("<li class=\"menu-item-has-children dropdown  name= \"" + name + "\" \" >");
+//            name++;
+//            stringBuilder.append("<a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\"\n" +
+//                    "aria-expanded=\"false\" ><i class=\"menu-icon fa " + createIconString("fa") + "\"></i>" + entry.getKey() + "</a>");
+//            //每一个种类还是一个ul 里面的每个页面是一个li
+//            stringBuilder.append(" <ul class=\"sub-menu children dropdown-menu \" >");
+//            //System.out.println(entry.getKey());style="padding-left: 0px"
+//            int ID = 1;
+//            for (Element element : entry.getValue().getChildren()) {
+//                if (element.getAttribute("url") != null) {
+//                    stringBuilder.append("<li id><i class=\"menu-icon " + createIconString("ti") + "\"></i><a href=\"" + element.getAttribute("url").getValue() + "\">" + element.getAttribute("name").getValue() + "</a></li>");
+//                    //System.out.println(element.getAttribute("name").getValue());
+//                } else {
+//                    stringBuilder.append("<li id><i class=\"menu-icon " + createIconString("ti") + "\"></i><a href=\"#\">" + element.getAttribute("name").getValue() + "</a></li>");
+//
+//                }
+//            }
+//            stringBuilder.append("</ul>");
+//            stringBuilder.append("</li>");
+//        }
+//        stringBuilder.append("</ul>");
+//        return stringBuilder.toString();
+//    }
 
-                }
-            }
-            stringBuilder.append("</ul>");
-            stringBuilder.append("</li>");
-        }
-        stringBuilder.append("</ul>");
-        return stringBuilder.toString();
-    }
-
-    public String createAsideString(String token) {
-        List<String> urlList = JdbcMysqlHelper.selectAuthority(token);
-
-
+    public String createAsideString(String token, String server) {
+        // List<String> urlList = JdbcMysqlHelper.selectAuthority(token);
+        List<String> urlList = DatabaseHelper.selectAuthority(token, server);
+        System.out.println("/////" + urlList);
         StringBuilder stringBuilder = new StringBuilder();
         //从外层div开始 div类是navigation-menu-body
         //这是最外层的ul
@@ -236,19 +193,33 @@ public class XmlMapping {
             }
             stringBuilder.append("<li> <a href=\"");
             if (entry.getValue().getAttribute("url") != null) {
-                stringBuilder.append(entry.getValue().getAttribute("authorization") + "\">");
+                stringBuilder.append(entry.getValue().getAttribute("authorization")).append("\">");
             } else {
                 stringBuilder.append("#\">");
             }
-            stringBuilder.append(entry.getKey() + "</a>");
-            stringBuilder.append("<ul>");
+            //iconList.add("data-feather=\"anchor\"");
+            stringBuilder.append("<i class=\"nav-link-icon\" data-feather=\"")
+                    .append(entry.getValue().getAttribute("icon").getValue())
+                    .append("\"></i>")
+                    .append(entry.getKey())
+                    .append("</a>")
+                    .append("<ul>");
             for (Element element : entry.getValue().getChildren()) {
                 if (!flag) {
                     if (!urlList.contains(element.getAttribute("url").getValue())) {
                         continue;
                     }
                 }
-                stringBuilder.append("<li id =\" " + element.getAttribute("name").getValue() + "\"><a href=\"" + element.getAttribute("url").getValue() + "\">" + element.getAttribute("name").getValue() + "</a></li>");
+                stringBuilder.append("<li id =\" ")
+                        .append(element.getAttribute("name").getValue())
+                        .append("\"><a href=\"")
+                        .append(element.getAttribute("url").getValue())
+                        .append("\">")
+                        .append(element.getAttribute("name").getValue())
+
+                        .append("</a>")
+
+                        .append("</li>");
 
             }
             stringBuilder.append("</ul>");
@@ -260,18 +231,30 @@ public class XmlMapping {
         return stringBuilder.toString();
     }
 
-    public List<String> createPageNameList() {
-        List<String> nameList = new ArrayList<>();
-        nameList.addAll(pageElement.keySet());
-        return nameList;
+
+    public List<String> createPageUrlList() {
+        List<String> UrlList = new ArrayList<>();
+        for (String string : pageElement.keySet()) {
+            UrlList.add(pageElement.get(string).getAttribute("url").getValue());
+        }
+        return UrlList;
     }
 
-    public List<String> createPageURLList() {
-        List<String> URLList = new ArrayList<>();
-        for (String string : pageElement.keySet()) {
-            URLList.add(pageElement.get(string).getAttribute("url").getValue());
+    public String createServerString(String selected) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<option value=\"0\"> 选择服务器 </option>");
+        for (String string : serverElement.keySet()) {
+            stringBuilder.append("<option value=\"")
+                    .append(serverElement.get(string).getAttribute("value").getValue())
+                    .append("\"");
+            if (selected.equals(serverElement.get(string).getAttribute("value").getValue())) {
+                stringBuilder.append(" selected");
+            }
+            stringBuilder.append(">")
+                    .append(serverElement.get(string).getValue())
+                    .append("</option>");
         }
-        return URLList;
+        return stringBuilder.toString();
     }
 
     public static void main(String[] args) throws JDOMException, IOException {
@@ -279,11 +262,9 @@ public class XmlMapping {
 
 
         XmlMapping xmlMapping = new XmlMapping();
-//        System.out.println("//////////////////////////////////////////////////////////////////////");
-//        System.out.println(xmlMapping.createPageString("queryLogin"));
-//        System.out.println(xmlMapping.createAsideString());
-        System.out.println(xmlMapping.createAsideString("root"));
-        System.out.println(xmlMapping.createPageURLList());
+
+        //System.out.println(xmlMapping.createAsideString("root"));
+        System.out.println(xmlMapping.createPageUrlList());
         System.out.println(xmlMapping.createPageString("operatorManage"));
 
     }
--- src/main/java/Server/Resources/MainResources.java
+++ src/main/java/Server/Resources/MainResources.java
@@ -1,7 +1,6 @@
 package Server.Resources;
 
 import Server.Automation.XmlMapping;
-import Server.DatabaseHelper.JdbcMysqlHelper;
 import Server.Verify.JwtUtils;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
@@ -9,15 +8,17 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.templ.thymeleaf.ThymeleafTemplateEngine;
-import io.vertx.reactivex.redis.client.Request;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
-
+@Slf4j
 public class MainResources extends AbstractVerticle {
 
     ThymeleafTemplateEngine thymeleafTemplateEngine;
     XmlMapping xmlMapping;
-    static String asideString ;
+    static String serverString;
+    static String asideString;
+
     public void registerResources(Router router, Vertx vertx) {
 //        router.get("/main").handler(this::main);
         try {
@@ -27,30 +28,65 @@ public class MainResources extends AbstractVerticle {
         }
 
         thymeleafTemplateEngine = ThymeleafTemplateEngine.create(vertx);
-        router.route().handler(StaticHandler.create());
-        List<String> pageList = xmlMapping.createPageURLList();
-        router.route("/main/home").handler(ctx->{
+        router.route("/main/home").handler(ctx -> {
             String token = JwtUtils.findToken(ctx);
-            asideString = xmlMapping.createAsideString(token);
+            //asideString = xmlMapping.createAsideString(token,);
+            //System.out.println("/////" + asideString);
+            serverString = xmlMapping.createServerString("0");
             var obj = new JsonObject();
-            obj.put("sidePanal", asideString);
-            obj.put("name", "");
+            obj.put("sidePanal", "");
+            obj.put("content", "");
+            obj.put("servers", serverString);
             thymeleafTemplateEngine.render(obj, "templates/home.html", bufferAsyncResult -> {
                 ctx.response().putHeader("content-type", "text/html").end(bufferAsyncResult.result());
             });
         });
-        for(String pageRouter: pageList ){
-            router.route("/main/"+pageRouter).handler(ctx->{
-                //asideString = xmlMapping.createAsideString(userName);
-                var obj = new JsonObject();
-                obj.put("sidePanal", asideString);
-                obj.put("pagename",pageRouter);
-                obj.put("name", xmlMapping.createElementString(xmlMapping.getElement(pageRouter)));
-                thymeleafTemplateEngine.render(obj, "templates/home.html", bufferAsyncResult -> {
-                    ctx.response().putHeader("content-type", "text/html").end(bufferAsyncResult.result());
-                });
+        router.route("/main/:serverRouter").handler(ctx -> {
+            String pageRouter = ctx.request().getParam("serverRouter");
+            asideString=xmlMapping.createAsideString(JwtUtils.findToken(ctx),ctx.request().getParam("serverRouter"));
+            var obj = new JsonObject();
+            obj.put("sidePanal", asideString);
+            obj.put("servers", serverString);
+            obj.put("content", "");
+            thymeleafTemplateEngine.render(obj, "templates/home.html", bufferAsyncResult -> {
+                ctx.response().putHeader("content-type", "text/html").end(bufferAsyncResult.result());
             });
-        }
+        });
+        router.route("/main/:serverRouter/:pageRouter").handler(ctx -> {
+            asideString=xmlMapping.createAsideString(JwtUtils.findToken(ctx),ctx.request().getParam("serverRouter"));
+            String pageRouter = ctx.request().getParam("pageRouter");
+            String serverRouter = ctx.request().getParam("serverRouter");
+            serverString = xmlMapping.createServerString(serverRouter);
+            String contentString;
+            if("0".equals(pageRouter)){
+                contentString="";
+            }else {
+                contentString=xmlMapping.createElementString(xmlMapping.getElement(ctx.request().getParam("pageRouter")));
+            }
+
+            var obj = new JsonObject();
+            obj.put("sidePanal", asideString);
+            obj.put("pagename", pageRouter);
+            obj.put("servers", serverString);
+            //obj.put("name", xmlMapping.createElementString(xmlMapping.getElement(pageRouter)));
+            obj.put("content", contentString);
+            thymeleafTemplateEngine.render(obj, "templates/home.html", bufferAsyncResult -> {
+                ctx.response().putHeader("content-type", "text/html").end(bufferAsyncResult.result());
+            });
+        });
+//        router.route("/main/:pageRouter").handler(ctx -> {
+//            //asideString = xmlMapping.createAsideString(userName);
+//            String pageRouter = ctx.request().getParam("pageRouter");
+//            var obj = new JsonObject();
+//            obj.put("sidePanal", asideString);
+//            obj.put("pagename", pageRouter);
+//            obj.put("servers", serverString);
+//            obj.put("name", xmlMapping.createElementString(xmlMapping.getElement(pageRouter)));
+//            thymeleafTemplateEngine.render(obj, "templates/home.html", bufferAsyncResult -> {
+//                ctx.response().putHeader("content-type", "text/html").end(bufferAsyncResult.result());
+//            });
+//        });
+
     }
 
 //    private void main(RoutingContext routingContext) {
