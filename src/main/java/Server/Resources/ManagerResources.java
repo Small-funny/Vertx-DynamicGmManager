32ba0c044930f60874981fc0c56c92260b23639b
jiahanyu <jiahanyu@fotoable>
2020-08-21 17:41:24 +0800

    feat: add feature

--- src/main/java/Server/Resources/ManagerResources.java
+++ src/main/java/Server/Resources/ManagerResources.java
@@ -1,6 +1,7 @@
 package Server.Resources;
 
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 /**
  * 管理员相关操作路由
@@ -8,6 +9,11 @@ import io.vertx.ext.web.Router;
 public class ManagerResources {
 
     public void registerResources(Router router) {
+        router.route("/manager/searchManagerInfo").handler(this::searchManagerInfo);
+    }
+
+    private void searchManagerInfo(RoutingContext routingContext) {
 
     }
+
 }
