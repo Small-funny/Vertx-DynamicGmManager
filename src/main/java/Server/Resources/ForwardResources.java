b5bd0c23c4983be4bd1985064e338295ebf0f485
wendaohan <wendaohan@fotoable.com>
2020-08-26 12:27:50 +0800

    feat add select n update

--- src/main/java/Server/Resources/ForwardResources.java
+++ src/main/java/Server/Resources/ForwardResources.java
@@ -9,6 +9,7 @@ import io.vertx.ext.web.client.WebClientOptions;
 
 import java.net.URLDecoder;
 import java.util.HashMap;
+import java.util.List;
 
 public class ForwardResources {
 
@@ -40,12 +41,22 @@ public class ForwardResources {
             String value = data.get(key);
             jsonObject.put(key, value);
         }
-
+        String url = jsonObject.getString("route");
+        System.out.println(url);
+        jsonObject.remove("route");
         webClient.post(8000, "localhost", "/GmServer")
                 .sendJsonObject(jsonObject, ar -> {
             if (ar.succeeded()) {
-                // System.out.println(ar.result().body());
-                routingContext.response().end(ar.result().body());
+//                System.out.println(ar.result().body());
+
+                //routingContext.response().end(ar.result().body());
+                String[] dataList = ar.result().body().toString().split("&");
+                String arg = null;
+                if (dataList.length != 1) {
+                    arg = dataList[dataList.length - 1];
+                }
+                System.out.println("arg" + arg);
+                routingContext.put("returnContent", dataList[0]).put("arg", arg).put("route",url).reroute("/main"+url);
             } else {
                 System.out.println("Wrong :" +ar.cause().getMessage());
                 routingContext.response().end("Operation failed !");
--- src/main/java/Server/Resources/MainResources.java
+++ src/main/java/Server/Resources/MainResources.java
@@ -6,8 +6,11 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.templ.thymeleaf.ThymeleafTemplateEngine;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
 
 @Slf4j
 public class MainResources extends AbstractVerticle {
@@ -40,44 +43,38 @@ public class MainResources extends AbstractVerticle {
             });
         });
         router.route("/main/:serverRouter/:pageRouter").handler(ctx -> {
-            asideString=xmlMapping.createAsideString(JwtUtils.findToken(ctx),ctx.request().getParam("serverRouter"));
+            asideString = xmlMapping.createAsideString(JwtUtils.findToken(ctx), ctx.request().getParam("serverRouter"));
             String pageRouter = ctx.request().getParam("pageRouter");
             String serverRouter = ctx.request().getParam("serverRouter");
+            //String returnContent=ctx.request().getParam("returnContent");
+            String returnContent =  ctx.get("returnContent");
+            String selectName= ctx.get("arg");
+            //System.out.println(returnContent);
             serverString = xmlMapping.createServerString(serverRouter);
             String contentString;
-            if("0".equals(pageRouter)){
-                contentString="";
-            }else {
-                contentString=xmlMapping.createElementString(xmlMapping.getElement(ctx.request().getParam("pageRouter")));
+            if ("0".equals(pageRouter)) {
+                contentString = "";
+            } else {
+                contentString = xmlMapping.createElementString(xmlMapping.getElement(ctx.request().getParam("pageRouter")));
             }
-
             var obj = new JsonObject();
             obj.put("sidePanal", asideString);
             obj.put("pagename", pageRouter);
             obj.put("servers", serverString);
-            obj.put("servername",serverRouter);
+            obj.put("servername", serverRouter);
+            obj.put("route","/"+serverRouter+"/"+pageRouter);
+
+
+
+            obj.put("returnContent",returnContent);
+            obj.put("selectName",selectName);
+
             //obj.put("name", xmlMapping.createElementString(xmlMapping.getElement(pageRouter)));
             obj.put("content", contentString);
             thymeleafTemplateEngine.render(obj, "templates/home.html", bufferAsyncResult -> {
                 ctx.response().putHeader("content-type", "text/html").end(bufferAsyncResult.result());
             });
         });
-//        router.route("/main/:pageRouter").handler(ctx -> {
-//            //asideString = xmlMapping.createAsideString(userName);
-//            String pageRouter = ctx.request().getParam("pageRouter");
-//            var obj = new JsonObject();
-//            obj.put("sidePanal", asideString);
-//            obj.put("pagename", pageRouter);
-//            obj.put("servers", serverString);
-//            obj.put("name", xmlMapping.createElementString(xmlMapping.getElement(pageRouter)));
-//            thymeleafTemplateEngine.render(obj, "templates/home.html", bufferAsyncResult -> {
-//                ctx.response().putHeader("content-type", "text/html").end(bufferAsyncResult.result());
-//            });
-//        });
 
     }
-
-//    private void main(RoutingContext routingContext) {
-//        routingContext.response().setStatusCode(HttpResponseStatus.OK.code()).sendFile("templates/index.html");
-//    }
 }
