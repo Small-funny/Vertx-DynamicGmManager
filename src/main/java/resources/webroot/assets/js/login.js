66dd0aa5bacef4e48f89aa73ffbd760e0c65173b
wendaohan <wendaohan@fotoable.com>
2020-10-10 10:25:37 +0800

    style: external js

---
+++ src/main/java/resources/webroot/assets/js/login.js
@@ -0,0 +1,103 @@
+
+function btn() {
+
+    document.getElementById("sign in").innerHTML =
+        "<span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span>\n" +
+        "            <span class=\"sr-only\">Loading...</span>"
+
+    $.ajax({
+        type: "GET",
+        datatype: "text",
+        url: "/login/verifyCode",
+        success: function (data) {
+            if (($('#verifyCode').val() !== "")) {
+                if ($('#verifyCode').val().toLowerCase() === data.toLowerCase()) {
+                    getPublicKey();
+                } else if ($('#verifyCode').val().toLowerCase() !== data.toLowerCase()) {
+                    toastr.error("验证码错误")
+                    document.getElementById("sign in").innerHTML = "Sign in"
+                }
+            } else {
+                document.getElementById("sign in").innerHTML = "Sign in"
+            }
+            changeUrl()
+        }
+    })
+
+}
+
+function getPublicKey() {
+    $.ajax({
+        type: "GET",
+        datatype: "text",
+        url: "/login/publicKey",
+        success: function (data) {
+            // alert(data);
+            let encrypt = new JSEncrypt();
+            encrypt.setPublicKey(data);
+
+            let usernameEncrypted = encrypt.encrypt($('#username').val())
+            let passwordEncrypted = encrypt.encrypt($('#password').val())
+
+            createToken(usernameEncrypted, passwordEncrypted)
+        }, error: function () {
+            document.getElementById("sign in").innerHTML = "Sign in"
+        }
+    })
+}
+
+function createToken(usernameEncrypted, passwordEncrypted) {
+    $.ajax({
+        type: "POST",
+        dataType: "text",
+        url: "/login/createToken",
+        data: JSON.stringify({
+            'username': usernameEncrypted,
+            'password': passwordEncrypted
+        }), success: function (data, result, xhr) {
+            if (xhr.status === 200) {
+                document.cookie = "Token=" + data + ";Path=/"
+                if (document.getElementById("remember").checked === true) {
+                    localStorage.setItem("username", $("#username").val());
+                    localStorage.setItem("password", $("#password").val());
+                } else {
+                    $("#password")
+                    localStorage.setItem("username", "");
+                    localStorage.setItem("password", "");
+                }
+                window.location.href = "/main/home"
+            }
+        }, error: function () {
+            document.getElementById("sign in").innerHTML = "Sign in"
+            toastr.error("账号或密码错误")
+        }
+    })
+}
+
+function changeUrl() {
+    document.getElementById('verifyCodePic').src = "/login/verifyCodePic" + "?time=" + Date.now();
+}
+
+window.onload = function () {
+
+    history.pushState(null, null, "/login");
+
+    if (localStorage.getItem("username") !== "" && localStorage.getItem("password") !== "") {
+        $("#username").val(localStorage.getItem("username"));
+        $("#password").val(localStorage.getItem("password"));
+        document.getElementById("remember").checked = true;
+    } else {
+        $("#username").val("");
+        $("#password").val("");
+        document.getElementById("remember").checked = false;
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "/login/authenticity",
+        success: function () {
+            window.location.href = "/main/home"
+        }
+    })
+    toastr.info("Welcome to fotoable")
+}
